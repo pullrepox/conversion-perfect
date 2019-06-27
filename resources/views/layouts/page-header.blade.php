5714448124b3c8d15c2e2f0edda2c527c1d3549a
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-06-27 23:23:22 +0300

    Feedback #6 [bi] fix

--- resources/views/layouts/page-header.blade.php
+++ resources/views/layouts/page-header.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="header-body">
             <div class="row align-items-center py-3">
-                <div class="col-lg-6 col-7">
+                <div class="col-lg-12">
                     <h6 class="h2 text-white d-inline-block mb-0">{{ $data['main_name'] }}</h6>
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
@@ -22,15 +22,6 @@
                             </li>
                         </ol>
                     </nav>
-                </div>
-                <div class="col-lg-6 col-5 text-right">
-                    @if ($data['button_show'])
-                        @foreach($data['button_data'] as $row)
-                            @if ($row['button_url'] != '' && $row['button_text'] != '')
-                                <a href="{{ $row['button_url'] }}" class="btn btn-sm btn-neutral">{!! $row['button_text'] !!}</a>
-                            @endif
-                        @endforeach
-                    @endif
                 </div>
             </div>
         </div>
