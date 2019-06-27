2f60049418d99d0afa552cb029f1758725af57bf
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-06-27 09:38:07 +0300

    content option preview stuff changed

--- resources/views/users/bars-partials/bars-preview.blade.php
+++ resources/views/users/bars-partials/bars-preview.blade.php
@@ -16,12 +16,12 @@
             <div class="w-100 mt--4" v-if="model.position === 'bottom'">
                 <img src="{{asset('assets/img/mockup.gif')}}" alt="" class="img-fluid w-100">
             </div>
-            <div style="width:100%; height: 150px; font-size: 20px; font-family: 'Nunito', sans-serif; color: rgb(255, 255, 255); text-align: right;"
+            <div style="width:100%; min-height: 150px; font-size: 20px; font-family: 'Nunito', sans-serif; color: rgb(255, 255, 255); text-align: right;position: relative;"
                  :style="{'background': model.background_color}">
-                <div style="height:138px; width:100%; font-size:20px; font-family: 'Nunito', sans-serif; display: flex; align-items: center; justify-content: center;">
-                    <div style="display:inline-block; vertical-align:top; width:auto; min-width:60%; height:150px; text-align:center;">
-                        <div style="display:inline-block; width:auto; height:150px;">
-                            <div :style="{ color: model.headline_color }" style="width: 100%; min-width: 100%; line-height: 70px; margin:0 auto; font-size: 1.6rem;padding-top: 15px;">
+                <div style="min-height:138px; width:100%; font-size:20px; font-family: 'Nunito', sans-serif; display: flex; align-items: center; justify-content: center;">
+                    <div style="display:inline-block; vertical-align:top; width:auto; min-width:60%; min-height:150px; text-align:center;">
+                        <div style="display:inline-block; width:auto; min-height: 150px;">
+                            <div :style="{ color: model.headline_color }" style="width: 100%; min-width: 100%; line-height: 60px; margin:0 auto; font-size: 1.6rem;padding-top: 15px;">
                                 <span v-for="(h_l, h_i) in model.headline" :key="`hLine_attr_${h_i}`" v-if="h_l.insert.trim() != ''">
                                     <span v-if="h_l.attributes" :style="{
                                         'font-weight': h_l.attributes.bold ? 'bold' : 'normal',
@@ -36,7 +36,7 @@
                                 </span>
                             </div>
                             <div :style="{ color: model.content.sub_headline_color, 'background': model.content.sub_background_color }"
-                                 style="width: 100%; min-width: 100%; line-height: 26px; margin: 0 auto; font-size: 16px; padding: 5px;"
+                                 style="width: 100%; min-width: 100%; margin: 0 auto; font-size: 16px; padding: 5px;"
                                  v-if="model.content.sub_headline[0].insert.trim() != ''">
                                 <span v-for="(s_h_l, s_h_i) in model.content.sub_headline" :key="`s_hLine_attr_${s_h_i}`" v-if="s_h_l.insert.trim() != ''">
                                     <span v-if="s_h_l.attributes" :style="{
@@ -54,7 +54,8 @@
                         </div>
                     </div>
                 </div>
-                <div style="font-size: 12px; line-height: 3px; font-family: Oswald; padding-right: 5px;" :style="{ color: model.headline_color }">
+                <div style="font-size: 12px; line-height: 20px; font-family: Oswald; padding-right: 5px;position: absolute;bottom: 0;right: 0;width: auto;z-index: 100;"
+                     :style="{ color: model.headline_color }">
                     POWERED BY <a style="color:inherit; text-decoration:inherit;text-transform: uppercase;" href="//app.conversionperfect.com" target="_BLANK">{{ config('app.name') }}</a>
                 </div>
             </div>
