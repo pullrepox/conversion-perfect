9a28a2e12a36f3a35641dfa36486cedfc45c2b82
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-07-04 00:11:39 +0300

    avatar

--- resources/views/users/trainings.blade.php
+++ resources/views/users/trainings.blade.php
@@ -23,12 +23,12 @@
                         <div class="col-md-6 mt-1">
                             <div class="card">
                                 <!-- Card image -->
-                                <div class="w-100">
-                                    <iframe class="card-img-top" width="100%" height="100%" style="border: 0; min-height: 450px;width: 100%;"
+                                <div class="card-img-top embed-responsive embed-responsive-16by9">
+                                    <iframe class="card-img-top embed-responsive-item"
                                             src="{{ $row->video_url }}" allow="autoplay; fullscreen;" allowfullscreen></iframe>
                                 </div>
                                 <!-- Card body -->
-                                <div class="card-body">
+                                <div class="card-body" style="height: 175px;">
                                     <h5 class="h2 card-title mb-0">{{ $row->title }}</h5>
                                     <p class="card-text mt-4">{{ $row->description }}</p>
                                 </div>
