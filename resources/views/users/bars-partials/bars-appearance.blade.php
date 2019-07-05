384911539ebbc282b2bb2bb1ca4c22f7e48da175
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-07-06 01:27:51 +0300

    Feedback #26 fix

--- resources/views/users/bars-partials/bars-appearance.blade.php
+++ resources/views/users/bars-partials/bars-appearance.blade.php
@@ -35,11 +35,6 @@
                     </div>
                     <div class="col-md-2 col-sm-3 pl-0 mt--1">@{{ model.appearance.opacity }}%</div>
                     <input type="hidden" name="opacity" v-model="model.appearance.opacity"/>
-                    @error('opacity')
-                    <span class="invalid-feedback" style="display: block;" role="alert">
-                        {{ $message }}
-                    </span>
-                    @enderror
                 </div>
             </div>
         </div>
