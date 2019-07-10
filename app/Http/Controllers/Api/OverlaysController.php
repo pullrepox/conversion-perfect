c7cc66ffbeed43f7495f9d7f509c9893828ff448
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-07-10 04:36:52 +0300

    preview html page

--- app/Http/Controllers/Api/OverlaysController.php
+++ app/Http/Controllers/Api/OverlaysController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\BarsRepository;
 use App\User;
-use Illuminate\Http\Request;
 
 class OverlaysController extends Controller
 {
@@ -16,7 +15,7 @@ class OverlaysController extends Controller
         $this->barRepo = $barsRepository;
     }
     
-    public function index($sub_domain, $link_name, Request $request)
+    public function index($sub_domain, $link_name)
     {
         $user = User::where('subdomain', $sub_domain)->first();
         $bar = $this->barRepo->model()->where('user_id', $user->id)->where('custom_link_text', $link_name)->first();
