5941c4cf3bca6a246f8aedab915dbfb02149bac2
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-07-12 11:23:03 +0300

    javascript to display bar

--- app/Http/Controllers/Api/OverlaysController.php
+++ app/Http/Controllers/Api/OverlaysController.php
@@ -38,9 +38,8 @@ class OverlaysController extends Controller
             $html_code = view('users.track-partials.script', compact('bar'));
             $code = TinyMinify::html($html_code);
             
-            return view('users.track-partials.preview-scripts', compact('code'), [
-                'Content-Type' => 'application/javascript; charset=utf-8;'
-            ]);
+            header('Content-Type: application/javascript; charset=utf-8;');
+            return view('users.track-partials.preview-scripts', compact('code'));
         } else {
             abort(404, 'No existing is matched Conversion Bar.');
         }
