039c592316912009dc4184ae1cccf5f38135298f
clickperfect <aleksandr.danilov.ua@outlook.com>
2019-07-12 11:45:50 +0300

    javascript to display bar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
-header('x-frame-options: *');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
